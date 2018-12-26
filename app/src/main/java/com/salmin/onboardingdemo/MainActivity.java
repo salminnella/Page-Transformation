fe9e292b0fb3000ebf87bfd993550d6be36a7e26
salminnella@gmail.com <salminnella@gmail.com>
2018-12-26 13:57:03 -0800

    removed page changed listener for view pager

--- app/src/main/java/com/salmin/onboardingdemo/MainActivity.java
+++ app/src/main/java/com/salmin/onboardingdemo/MainActivity.java
@@ -30,42 +30,5 @@ public class MainActivity extends AppCompatActivity {
 
         // Set a PageTransformer
         mViewPager.setPageTransformer(false, new IntroPageTransformer());
-
-        mViewPager.addOnPageChangeListener(new OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int position, float positionOffset,
-                                       int positionOffsetPixels) {
-                if(position < (introAdapter.getCount() -1) && position < (colors.length - 1)) {
-
-                    Log.d(TAG, "onPageScrolled: setBackgroundColor ");
-                    Log.d(TAG, "----------------------------------------------");
-                    mViewPager.setBackgroundColor((Integer) argbEvaluator.evaluate(positionOffset, colors[position], colors[position + 1]));
-
-                } else {
-                    Log.d(TAG, "onPageScrolled: else color *********************");
-                    mViewPager.setBackgroundColor(colors[colors.length - 1]);
-
-                }
-            }
-
-            @Override
-            public void onPageSelected(int position) {
-                Log.d(TAG, "onPageSelected: was called - position = " + position);
-//                switch (position) {
-//                    case 0:
-//                        mViewPager.setBackgroundColor(getResources().getColor(R.color.page1));
-//                        break;
-//                    case 1:
-//                        mViewPager.setBackgroundColor(getResources().getColor(R.color.page1));
-//                        break;
-//                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
-
     }
 }
